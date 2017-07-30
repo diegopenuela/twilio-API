0ce45b1f0a5f13218efff827c1ab5818cf6f902a
diegopenuela <diego.penuela@gmail.com>
2017-07-30 04:05:14 -0500

    Basic config twilio

--- send_sms.py
+++ send_sms.py
@@ -3,10 +3,10 @@
 from twilio.rest import Client
 
 # Find these values at https://twilio.com/user/account
-account_sid = "ACXXXXXXXXXXXXXXXXX"
-auth_token = "YYYYYYYYYYYYYYYYYY"
+account_sid = "ACec719e187bb196bd278f412264266280"
+auth_token = "96d1db7a4ced24cee7b3e77b3d22669c"
 client = Client(account_sid, auth_token)
 
-message = client.api.account.messages.create(to="+12316851234",
+message = client.api.account.messages.create(to="+573162697465",
                                              from_="+15555555555",
-                                             body="Hello there!")
+                                             body="Recuerda que tu impuesto IVA Bimensual (Mayo/Junio) se vence en 3 dias. Servicio gratuito de BackStartup.")
